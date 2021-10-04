3013f53bbc8f67d8ca4495aa98ddd652cce007cc
AE-0 <johannfvm@gmail.com>
2021-10-03 23:27:31 -0300

    flechas simples y arreglo de errores
    Intento de función de caminos y tipo de nodos

--- js/script.js
+++ js/script.js
@@ -12,34 +12,32 @@ var vinculos = [
 */
 
 // Grafo vacio
-var nodos = [];
-var vinculos = [];
-
+var nodos = [], vinculos = [];
 
 var source = [], target = [];
 var ultimoNodo = nodos.length;
-let auxMatrix = [];
-
+let auxMatrix = [], matricita = [], matrioska = [];
+var columm = null;
+var colores =  d3.schemeDark2;
+const uuid = Math.floor(Math.random() * 1e9);
+var mousedownNode = null;
+var tool = null, seleccion = null;
+var yoffset = 42, xoffset = 177;
+var w = window.innerWidth - xoffset, h = window.innerHeight - yoffset, radio = 12;
+var conexion = 0, caminocta = 0;
 
 var svg = d3.select(".espacio")
+svg.attr("width", w).attr("height", h);
 svg.on("contextmenu", function() {
   d3.event.preventDefault();
 });
-
-var yoffset = 42;
-var xoffset = 293;
-var w = window.innerWidth - xoffset, h = window.innerHeight - yoffset, radio = 10;
-svg.attr("width", w).attr("height", h);
 
 var dragLine = svg
   .append("path")
   .attr("class", "dragLine hidden")
   .attr("d", "M0,0L0,0");
-
-
-const uuid = Math.floor(Math.random() * 1e9);
-
-svg
+  
+var flecha = svg
   .append('defs')
   .append('marker')
   .attr('id', `arrowhead-${uuid}`)
@@ -57,8 +55,6 @@ svg
 
 var aristas = svg.append("g").selectAll(".arista");
 var vertices = svg.append("g").selectAll(".vertice");
-var textos = svg.append("g").selectAll(".text");
-
 
 var force = d3
   .forceSimulation()
@@ -76,9 +72,6 @@ var force = d3
 
 force.nodes(nodos);
 force.force("link").links(vinculos);
-
-var mousedownNode = null;
-var tool = null;
 
 var limpiarBtn = document.querySelector(".limpiar");
 limpiarBtn.addEventListener("click", limpiarTodo);
@@ -102,6 +95,7 @@ var visualizarBtn = document.querySelector(".visualizar");
 visualizarBtn.addEventListener("click", e => {
   modalIng.style.display = "block";
 });
+// Input Visualizar
 var inputGrafos = document.querySelector('input[name="ginput"]')
 inputGrafos.addEventListener("onkeypress", e => {
   e.preventDefault();
@@ -110,9 +104,8 @@ inputGrafos.addEventListener("onkeypress", e => {
 var submit = document.querySelector('button[type="submit"]')
 submit.addEventListener("click", ingresoDatos)
 
-var stringcito;
-
 // Ingreso de nodos por texto
+var stringcito;
 function ingresoDatos() {
   stringcito = inputGrafos.value;
   var stringNodos = inputGrafos.value;
@@ -120,107 +113,23 @@ function ingresoDatos() {
   regexRule = /\[[0-9],[0-9]\]/;
   var arrayNodos = [...stringcito.match(regexRule)]
   console.log(arrayNodos);
-
 }
 
-// Boton Propiedades ("temporal")
-var propiedadesBtn = document.querySelector(".propiedades");
-propiedadesBtn.addEventListener("click", propiedades);
-
-var columm = null;
-
-var matricita = [];
-var matrioska = [];
-
-function propiedades() {
-  var nAristas = document.querySelector(".naristas");
-  nAristas.innerHTML = "Aristas: " + vinculos.length;
-  var nVertices = document.querySelector(".nvertices");
-  nVertices.innerHTML = "Vertices: " + nodos.length;
-  nVertices.addEventListener("mouseenter", e => {
-    document.querySelectorAll(".vertice").forEach(function(d){d.setAttribute("class", "vertice seleccionado");})
-    setTimeout(function(){
-      document.querySelectorAll(".vertice").forEach(function(d){d.setAttribute("class", "vertice");})
-    }, 500)
-  })
-  nAristas.addEventListener("mouseenter", e => {
-    document.querySelectorAll(".arista").forEach(function(d){d.setAttribute("class", "arista seleccionado");})
-    setTimeout(function(){
-      document.querySelectorAll(".arista").forEach(function(d){d.setAttribute("class", "arista");})
-    }, 500)
-  })
-
-  var barraderecha = document.querySelector(".derecha");
-  barraderecha.style.height = "calc(100% - 45px)";
-
-
-  for (let index = 0; index < vinculos.length; index++) {
-    columm = vinculos[index];
-    source.push(columm.source.index);
-    target.push(columm.target.index);
-    auxMatrix.push([index]);
-  }
-  matricita.push(source);
-  matricita.push(target);
-
-  for (let index = 0; index < nodos.length; index++) {
-    auxMatrix[index] = source[index] + "," + target[index];
-  }
-  
-  //Llena una matriz con 0's
-  for (let index = 0; index < nodos.length; index++) {
-    matrioska[index] = Array(nodos.length).fill(0)
-  }
-  
-  //Matriz funcionando
-  for (let index = 0; index <= nodos.length - 1; index++) {
-    for (let jndex = 0; jndex <= nodos.length - 1; jndex++) {
-      for (let kndex = 0; kndex <= source.length - 1; kndex++) {
-        for (let lndex = 0; lndex <= matricita.length - 1; lndex++) {
-          var m = matricita[lndex][kndex];
-          if (lndex == 0) {
-            mm = m;
-          }
-        }
-        matrioska[mm][m] = 1;
-      }
-      break;
-    }
-  }
-
-  var tabla= "<table border=\"0\">";
-     
-  tabla += "<tr><td></td>";
-  for(let jndex = 0; jndex<nodos.length; jndex++) { 
-      tabla += "<td>" + (jndex + 1) + "</td>";
-  }
-  tabla+="</tr>";
-  
-  for(let index = 0; index < nodos.length; index++) {
-      tabla += "<tr>";
-      tabla += "<td>" + (index+1) + "</td>";
-      for(let jndex = 0; jndex < nodos.length; jndex++) { 
-          tabla += "<td>" + matrioska[index][jndex] + "</td>";
-      }
-      tabla += "</tr>";
-  }
-  tabla += "</table>";
-
-  document.querySelector(".matriz").innerHTML=tabla;
-}
-
+// Checkbox Numeros
 var numLabel = document.querySelector('input[name="numeros"]');
-numLabel.addEventListener("click", e => {
+numLabel.addEventListener("click", mostrarNum);
+function mostrarNum() {
   if (numLabel.checked == true) {
-    textos.style("display", "inline");
+    d3.selectAll(".texto").style("display", "inline");
   } else {
-    textos.style("display", "none");
+    d3.selectAll(".texto").style("display", "none");
   }
-});
+}
 
-
+// Checkbox Flechas
 var flechaCheck = document.querySelector('input[name="flechas"]');
-flechaCheck.addEventListener("click", e => {
+flechaCheck.addEventListener("click", mostrarFlechas);
+function mostrarFlechas() {
   if (flechaCheck.checked == true) {
     document.querySelectorAll(".arista").forEach(function (d) {
       d.setAttribute("marker-end", "url(#arrowhead-" + uuid + ")");
@@ -231,7 +140,8 @@ flechaCheck.addEventListener("click", e => {
       d.setAttribute("marker-end", "");
     })
   }
-});
+}
+
 // Boton Crear 
 var crearBtn = d3.select('button[name="crear"]');
 crearBtn.on("click", function(d) {
@@ -242,8 +152,6 @@ crearBtn.on("click", function(d) {
     .on("mouseup", hideDragLine)
     .on("mouseleave", hideDragLine);
 });
-
-var seleccion = null;
 
 // Boton Mover
 var moverBtn = d3.select('button[name="mover"]');
@@ -264,7 +172,6 @@ borrarBtn.on("click", function(d) {
 //  aristas.on("mousedown", borrarNodo);
 });
 
-
 // Actualiza la simulación
 function tick() {
   aristas
@@ -281,25 +188,9 @@ function tick() {
       return d.target.y;
     });
 
-  vertices
-    .attr("cx", function(d) {
-      return d.x;
-    })
-    .attr("cy", function(d) {
-      return d.y;
-    });
-  textos
-    .attr("x", function(d) {
-      if (d.id < 10) {
-        return d.x - 4;
-      } else {
-        return d.x - 9;
-      }
-    })
-    .attr("y", function(d) {
-      return d.y + 5;
-    });
-
+  vertices.attr("transform", function(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+  });
 }
 
 // Vacia el grafo
@@ -315,23 +206,25 @@ function limpiarTodo() {
 function cambioTool(toolname) {
   var toolBtn1 = 'button[name="', toolBtn2= '"]';
   tool = toolBtn1 + toolname + toolBtn2;
-  document.querySelectorAll(".tool").forEach(function(d) {
-    d.setAttribute("class", "tool");
-  });
-  document.querySelector(tool).setAttribute("class", "tool activo");
+  d3.selectAll(".tool").classed("activo", false);
+  d3.select(tool).classed("activo", true);
+  tool = toolname;
+  
   svg
-    .on("mousedown", null)
-    .on("mousemove", null)
-    .on("mouseup", null);
+  .on("mousedown", null)
+  .on("mousemove", null)
+  .on("mouseup", null);
+  
   vertices
     .on("mousedown", null)
-    .on("mousemove", null)
-    .on("mouseup", null);
+    //.on("mousemove", null)
+    //.on("mouseup", null);
+
   aristas
     .on("mousedown", null)
-    .on("mousemove", null)
+    //.on("mousemove", null)
     .on("mouseup", null);
-  tool = toolname;
+
   restart();
 }
 
@@ -346,10 +239,7 @@ function añadirNodo() {
 }
 
 function borrarNodo(d, i) {  
-
   nodos.splice(nodos.indexOf(d), 1);
-  var texto1 = '.texto[id="', texto2= '"]';
-  var textoB = texto1 + nodos.indexOf(d) + texto2;
   var vinculosToRemove = vinculos.filter(function(l) {
     return l.source === d || l.target === d;
   });
@@ -357,7 +247,6 @@ function borrarNodo(d, i) {
     vinculos.splice(vinculos.indexOf(l), 1);
   });
   d3.event.preventDefault();
-  d3.select(textoB).remove()
   restart();
 }
 
@@ -370,7 +259,6 @@ function removeEdge(d, i) {
 function beginDragLine(d) {
   //to prevent call of añadirNodo through svg
   d3.event.stopPropagation();
-  //to prevent dragging of svg in firefox
   d3.event.preventDefault();
   mousedownNode = d;
   dragLine
@@ -407,23 +295,121 @@ function updateDragLine() {
 function hideDragLine() {
   dragLine.classed("hidden", true);
   mousedownNode = null;
-  restart();
 }
 
 function endDragLine(d) {
-  if (!mousedownNode || mousedownNode === d) return;
-  //return if link already exists
-  for (let index = 0; index < vinculos.length; index++) {
+  if (!mousedownNode || mousedownNode === d) return;   //Evita bug source.id = null y nodos vinculados así mismos  
+  for (let index = 0; index < vinculos.length; index++) {   //Evita crear vinculos ya existentes
     var l = vinculos[index];
-    if (
-      (l.source === mousedownNode && l.target === d) ||
-      (l.source === d && l.target === mousedownNode)
-    ) {
-      return;
-    }
+    if (l.source === mousedownNode && l.target === d) return;
   }
   var newLink = { source: mousedownNode, target: d };
   vinculos.push(newLink);
+  restart();
+}
+
+// Boton ("temporal")
+document.querySelector(".propiedades").addEventListener("click", propiedades);
+
+// Muestra las propiedades del grafo
+function propiedades() {
+  var nAristas = document.querySelector(".naristas");
+  nAristas.innerHTML = "Aristas: " + vinculos.length;
+  var nVertices = document.querySelector(".nvertices");
+  nVertices.innerHTML = "Vertices: " + nodos.length;
+
+  nVertices.addEventListener("mouseenter", e => {
+    document.querySelectorAll(".vertice").forEach(function(d){d.setAttribute("class", "vertice seleccionado");})
+    setTimeout(function(){
+      document.querySelectorAll(".vertice").forEach(function(d){d.setAttribute("class", "vertice");})
+    }, 500)
+  })
+  nAristas.addEventListener("mouseenter", e => {
+    document.querySelectorAll(".arista").forEach(function(d){d.setAttribute("class", "arista arselect");})
+    flecha.attr("stroke", "darkorange").attr("fill", "darkorange")
+    setTimeout(function(){
+      document.querySelectorAll(".arista").forEach(function(d){d.setAttribute("class", "arista");})
+      flecha.attr("stroke", "#999").attr("fill", "#999")
+    }, 500)
+  })
+
+  var barraderecha = document.querySelector(".derecha");
+  barraderecha.style.height = "calc(100% - 45px)";
+
+  for (let index = 0; index < vinculos.length; index++) {
+    columm = vinculos[index];
+    source.push(columm.source.index);
+    target.push(columm.target.index);
+    auxMatrix.push([index]);
+  }
+  matricita.push(source);
+  matricita.push(target);
+
+  for (let index = 0; index < nodos.length; index++) {
+    auxMatrix[index] = source[index] + "," + target[index];
+  }
+  
+  //Llena una matriz con 0's
+  for (let index = 0; index < nodos.length; index++) {
+    matrioska[index] = Array(nodos.length).fill(0)
+  }
+  
+  //Matriz funcionando
+  for (let jndex = 0; jndex <= nodos.length - 1; jndex++) {
+    for (let kndex = 0; kndex <= source.length - 1; kndex++) {
+      for (let lndex = 0; lndex <= matricita.length - 1; lndex++) {
+        var m = matricita[lndex][kndex];
+        if (lndex == 0) {
+          mm = m;
+        }
+      }
+      matrioska[mm][m] = 1;
+    }
+    break;
+  }
+  
+  var tabla= "<table border=\"0\">";
+     
+  tabla += "<tr><td></td>";
+  for(let jndex = 0; jndex<nodos.length; jndex++) { 
+      tabla += "<td>" + (jndex + 1) + "</td>";
+  }
+  tabla+="</tr>";
+  
+  for(let index = 0; index < nodos.length; index++) {
+      tabla += "<tr>";
+      tabla += "<td>" + (index+1) + "</td>";
+      for(let jndex = 0; jndex < nodos.length; jndex++) { 
+          tabla += "<td>" + matrioska[index][jndex] + "</td>";
+      }
+      tabla += "</tr>";
+  }
+  tabla += "</table>";
+
+  document.querySelector(".matriz").innerHTML=tabla;
+}
+
+// Solo funciona en grafos lineales
+function caminos(conexion)  {
+  while (conexion < vinculos.length) {
+      conexion = vinculos[conexion].target.id;
+      console.log(conexion);
+      return caminos(conexion - 1);
+  }
+  return;
+}
+
+function tipoGrafo() {
+  for (let index = 0; index <= vinculos.length; index++) { 
+    var jndex = index + 1;
+    if (vinculos[index].source.id == vinculos[jndex].target.id) { //Se rompe si el orden de ingreso de los vinculos no es el esperado
+      continue;
+    } else {
+      console.log("Tipo dirigido");
+      return;
+    }
+  }
+  console.log("Es simple");
 }
 
 // Actualiza el grafo, vinculos y nodos
@@ -449,49 +435,45 @@ function restart() {
 
   aristas = ar.merge(aristas);
 
+  flecha.style("display", mostrarFlechas)
+
   vertices = vertices.data(nodos, function(d) {
     return d.id;
   });
   vertices.exit().remove();
 
-  if (tool === "mover") {
-    var ve = vertices
-      .on("mousedown", function() {
-        ve.style("fill", "#57d733");
-      });
-  } else {
-    var ve = vertices
-      .enter()
-      .append("circle")
-      .attr("r", radio)
-      .attr("class", "vertice")
-      .style("fill", "#d73380") // Añadir funcion de colores
-      .on("mousedown", beginDragLine)
-      .on("mouseup", endDragLine)
-      .on("contextmenu", borrarNodo);
-
-    ve.append("title").text(function(d) {
-      return "v" + d.id;
-    });
-
-    vertices = ve.merge(vertices);
-  }
-
-  textos = textos.data(nodos, function(d) {
+  vertices.selectAll("text").text(function(d) {
     return d.id;
   });
 
-  var te = textos
+  var ve = vertices
     .enter()
-    .append("text")
-    .text(d => d.id)
-    .attr("class", "texto")
-    .attr("id", d => d.id)
+    .append("g")
+    .attr("class", "vertice")
     .on("mousedown", beginDragLine)
     .on("mouseup", endDragLine)
     .on("contextmenu", borrarNodo);
+    
+  ve.append("circle")
+    .attr("r", radio)
+    .style("fill", "#d73380") // Añadir funcion de colores
+    .append("title").text(function(d) {
+      return "v" + d.id;
+    });
+    
+    ve.append("text")
+    .attr("class", "texto")
+    .attr("x", function(d) {
+      if (d.id < 10) return - 4;
+       else return - 8;
+    })
+    .attr("y", 5)
+    .style("dislay", mostrarNum)
+    .text(function(d) {
+      return d.id;
+    });
 
-  textos = te.merge(textos);
+  vertices = ve.merge(vertices);
 
   force.nodes(nodos);
   force.force("link").links(vinculos);
